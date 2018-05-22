b8538c118d9294c85d02d843a93f6b79878c365d
AndySer37 <andy19970307@gmail.com>
2018-05-22 22:16:18 +0800

    update for 5/22

---
+++ 4/video.py
@@ -0,0 +1,11 @@
+import cv2
+video = cv2.VideoCapture('output.avi')
+while video.isOpened():
+    ret, frame = video.read()
+    if ret == False:
+        break
+    cv2.imshow('Play Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
